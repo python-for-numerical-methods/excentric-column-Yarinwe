8421ca7b5cdbd3e3ef549641ee3be1296d3087ea
Yarinwe <yarinwe@ac.sce.ac.il>
2026-06-01 01:30:51 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,3 +1,16 @@
+import numpy as np
+from scipy import optimize
+
+
+def column_stress_error(P, L, E, A, r, c, e, sigma_allow):
+    angle = (L / (2 * r)) * np.sqrt(P / (E * A))
+    secant = 1 / np.cos(angle)
+
+    sigma_max = (P / A) * (1 + (e * c / r**2) * secant)
+
+    return sigma_max - sigma_allow
+
+
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
     """
     L: אורך במ"מ
@@ -10,4 +23,9 @@ def find_critical_load(L, E, A, r, c, e, sigma_allow):
 
     Return: העומס P בניוטון (float)
     """
-    # כתבו כאן את הקוד
+    P_critical = optimize.newton(
+        lambda P: column_stress_error(P, L, E, A, r, c, e, sigma_allow),
+        500000
+    )
+
+    return float(P_critical)
